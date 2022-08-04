bfbb1131c7644970f7e8d17fd89922332cea4440
HQ\mspinks <mspinksosu@gmail.com>
2022-08-04 15:07:58 -0500

    One-Drive error. Pushing before I lose changes

--- GPIO/Interface and Generic Manager/IGPIO.c
+++ GPIO/Interface and Generic Manager/IGPIO.c
@@ -8,38 +8,11 @@
  * @file IGPIO.c
  * 
  * @details
- *      An interface for a GPIO library to be used with different processors.
- * 
- * Libraries that use this interface must implement the functions listed in the
- * interface, or function table. Since, there should only ever be one single
- * GPIO driver per processor, I am going to make the function table static. 
- * In order to set the function table, call the GPIO_SetDriverInterface 
- * and give it the function table for your processor specific code.
- * 
- * The functions here will use the base class object type. The functions in the
- * sub class will use the sub class object type. When the user calls a function
- * here, the function table, or interface, along with the base class object,
- * will determine which function to call and what sub class to give to the 
- * function.
- * 
- * In the example below, there are two init type objects. These do not have to
- * be kept in memory. They can be declared as local variables and used for each
- * pin's initialization.
- * 
- * Example Code:
- *      GPIO_DriverSetInterface(&MCU1_GPIOInterface);
- *      GPIO led1;
- *      GPIO_MCU1 myMcuLED1; // extends GPIO pin
- *      GPIOInitType init;
- *      GPIOInitType_MCU1 myMcuInit // extends GPIO init type
- *      init.type = GPIO_TYPE_DIGITAL_OUTPUT;
- *      init.pull = GPIO_PULL_NONE;
- *      myMcuInit.extendedClassMember = 1;
- *      GPIO_MCU1_CreateInitType(&init, &myMcuInit); // connect sub and base
- *      GPIO_MCU1_Create(&led1, &myMcuLED1); // connect sub class and base
- *      GPIO_InitPin(&led1, &init);
- *      GPIO_Set(&led1); // set output high
- *      GPIO_SetType(&led1, GPIO_TYPE_ANALOG); // ready pin for sleep
+ *      The counterpart for the GPIO interface library. Functions here are
+ * called using the base class object. When you use the create function, you
+ * are linking a base class and a subclass together. This file will use the
+ * interface and your base class object to determine which function
+ * implementation to call and which subclass object to give to it.
  * 
  ******************************************************************************/
 
@@ -61,68 +34,21 @@ static GPIOInterface *interface;
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
-/***************************************************************************//**
- * @brief Set the interface for this GPIO driver
- * 
- * Because there is usually only one GPIO driver per processor, there doesn't
- * need to be a pointer to the interface in each GPIO object. For this library
- * the interface will be static.
- * 
- * @param thisInterface  the function table you want to use
- */
 void GPIO_DriverSetInterface(GPIOInterface *thisInterface)
 {
     interface = thisInterface;
 }
 
-/***************************************************************************//**
- * @brief Combine the base class and sub class
- * 
- * Links the instance pointer in the base class to the sub class. Because of 
- * the void pointer, my preferred method is to call this function from the sub
- * class constructor. I created a sub class constructor that needs an instance 
- * of the sub class and base class. This makes the create function more type
- * safe.
- * 
- * @param self  pointer to the GPIO you are using
- * 
- * @param instanceOfSubClass  the child object that implements the GPIO
- * 
- */
 void GPIO_Create(GPIO *self, void *instanceOfSubClass)
 {
     self->instance = instanceOfSubClass;
 }
 
-/***************************************************************************//**
- * @brief Combine the base class and sub class
- * 
- * Links the instance pointer in the base class to the sub class. Because of 
- * the void pointer, my preferred method is to call this function from the sub
- * class constructor. I created a sub class constructor that needs an instance 
- * of the sub class and base class. This makes the create function more type
- * safe.
- * 
- * @param params  pointer to the GPIOInit type you are using
- * 
- * @param instanceOfSubClass  the child object that implements the GPIOInit
- */
 void GPIO_CreateInitType(GPIOInitType *params, void *instanceOfSubClass)
 {
     params->instance = instanceOfSubClass;
 }
 
-/***************************************************************************//**
- * @brief Set pointer to analog value
- * 
- * This is useful for mapping analog pins. The calling function doesn't need to
- * know which analog channel a pin is, just the name of the pin. Therefore, 
- * calling functions between two different processors can remain the same.
- * 
- * @param self  pointer to the GPIO you are using
- * 
- * @param ptr  pointer to the analog value
- */
 void GPIO_SetPointerToAnalogValue(GPIO *self, uint16_t *ptr)
 {
     self->ptrToAnalogValue = ptr;
@@ -134,13 +60,7 @@ void GPIO_SetPointerToAnalogValue(GPIO *self, uint16_t *ptr)
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
-/***************************************************************************//**
- * @brief Initialize a GPIO object
- * 
- * @param self  pointer to the GPIO you are using
- * 
- * @param params  pointer to the GPIOInit type you are using
- */
+
 void GPIO_InitPin(GPIO *self, GPIOInitType *params)
 {
     if(interface->GPIO_InitPin != NULL && self->instance != NULL 
@@ -150,11 +70,7 @@ void GPIO_InitPin(GPIO *self, GPIOInitType *params)
     }
 }
 
-/***************************************************************************//**
- * @brief Set a pin high
- * 
- * @param self  pointer to the GPIO you are using
- */
+
 void GPIO_SetPin(GPIO *self)
 {
     if(interface->GPIO_SetPin != NULL && self->instance != NULL)
@@ -163,11 +79,7 @@ void GPIO_SetPin(GPIO *self)
     }
 }
 
-/***************************************************************************//**
- * @brief Clear a pin
- * 
- * @param self  pointer to the GPIO you are using
- */
+
 void GPIO_ClearPin(GPIO *self)
 {
     if(interface->GPIO_ClearPin != NULL && self->instance != NULL)
@@ -176,11 +88,7 @@ void GPIO_ClearPin(GPIO *self)
     }
 }
 
-/***************************************************************************//**
- * @brief Invert a pin's value
- * 
- * @param self  pointer to the GPIO you are using
- */
+
 void GPIO_InvertPin(GPIO *self)
 {
     if(interface->GPIO_InvertPin != NULL && self->instance != NULL)
@@ -189,13 +97,7 @@ void GPIO_InvertPin(GPIO *self)
     }
 }
 
-/***************************************************************************//**
- * @brief Write a value to a pin
- * 
- * @param self  pointer to the GPIO you are using
- * 
- * @param setPinHigh  true = high, false = low
- */
+
 void GPIO_WritePin(GPIO *self, bool setPinHigh)
 {
     if(interface->GPIO_WritePin != NULL && self->instance != NULL)
@@ -204,16 +106,7 @@ void GPIO_WritePin(GPIO *self, bool setPinHigh)
     }
 }
 
-/***************************************************************************//**
- * @brief Read the value of a pin
- * 
- * If the pin is analog and the pointer is set, it will return the analog value.
- * Otherwise, return either a 1 or 0. Works with boolean logic.
- * 
- * @param self  pointer to the GPIO you are using
- * 
- * @return uint16_t  adc reading if analog, if digital 0 = low, 1 = high
- */
+
 uint16_t GPIO_ReadPin(GPIO *self)
 {
     if(interface->GPIO_ReadPin != NULL && self->instance != NULL)
@@ -226,16 +119,7 @@ uint16_t GPIO_ReadPin(GPIO *self)
     }
 }
 
-/***************************************************************************//**
- * @brief Read the boolean value of a pin
- * 
- * An alternative to the ReadPin function. If the pin is digital, then return 
- * either true or false
- * 
- * @param self  pointer to the GPIO you are using
- * 
- * @return true if digital and pin is high, otherwise false
- */
+
 bool GPIO_ReadBool(GPIO *self)
 {
     if(interface->GPIO_ReadBool != NULL && self->instance != NULL)
@@ -248,16 +132,7 @@ bool GPIO_ReadBool(GPIO *self)
     }
 }
 
-/***************************************************************************//**
- * @brief Set the pin type
- * 
- * Set the pin's asscociated registers
- * 
- * @param self  pointer to the GPIO you are using
- * 
- * @param type  GPIO_TYPE_ANALOG, GPIO_TYPE_DIGITAL_INPUT, 
- *              GPIO_TYPE_DIGITAL_OUTPUT, GPIO_TYPE_OPEN_DRAIN
- */
+
 void GPIO_SetType(GPIO *self, GPIOType type)
 {
     if(interface->GPIO_SetType != NULL && self->instance != NULL)
@@ -266,16 +141,7 @@ void GPIO_SetType(GPIO *self, GPIOType type)
     }
 }
 
-/***************************************************************************//**
- * @brief Get the type of pin
- * 
- * Read the pin's asscociated registers and determine the pin type
- * 
- * @param self  pointer to the GPIO you are using
- * 
- * @return GPIOType  GPIO_TYPE_ANALOG, GPIO_TYPE_DIGITAL_INPUT, 
- *                   GPIO_TYPE_DIGITAL_OUTPUT, GPIO_TYPE_OPEN_DRAIN
- */
+
 GPIOType GPIO_GetType(GPIO *self)
 {
     if(interface->GPIO_GetType != NULL && self->instance != NULL)
@@ -288,15 +154,7 @@ GPIOType GPIO_GetType(GPIO *self)
     }
 }
 
-/***************************************************************************//**
- * @brief Set the pull up resistors
- * 
- * Set the pin's asscociated registers
- * 
- * @param self  pointer to the GPIO you are using
- * 
- * @param pullType  GPIO_PULL_UP, GPIO_PULL_DOWN, or GPIO_PULL_NONE
- */
+
 void GPIO_SetPull(GPIO *self, GPIOPull pullType)
 {
     if(interface->GPIO_SetPull != NULL && self->instance != NULL)
@@ -305,15 +163,7 @@ void GPIO_SetPull(GPIO *self, GPIOPull pullType)
     }
 }
 
-/***************************************************************************//**
- * @brief Get the pull up resistors if any
- * 
- * Read the pin's associated registers and determine the pull up type
- * 
- * @param self  pointer to the GPIO you are using
- * 
- * @return GPIOPull  GPIO_PULL_UP, GPIO_PULL_DOWN, or GPIO_PULL_NONE
- */
+
 GPIOPull GPIO_GetPull(GPIO *self)
 {
     if(interface->GPIO_GetPull != NULL && self->instance != NULL)
