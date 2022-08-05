47f238d42ad503aad2e06559cbb60de4ab99e88f
HQ\mspinks <mspinksosu@gmail.com>
2022-08-05 12:19:22 -0500

    Updated to match new style. Changed init function
    Center-off feature can now be enabled and disabled on the fly. I'm not sure why anyone would want to do this, but there it is.

--- Switch/Switch.c
+++ Switch/Switch.c
@@ -13,21 +13,6 @@
  * Optional "center off" that allows output to be off. The default is to not 
  * allow the output to be off.
  * 
- * The inputs will be debounced for you. When you initialize the Switch object,
- * you give it a time in milliseconds. This is the time that you plan to 
- * periodically call the tick function.
- * 
- * If you would like to use this for a single pole switch, simply set one of
- * the inputs to always be false when you call the tick function.
- * 
- * Every function will use Switch as its first argument. This is the instance
- * of the Switch you created. You should not need to manipulate any data inside
- * the Switch object. I have provided functions to do this for you.
- * 
- * There are flags for different events such as "A on", "B on", as well as any 
- * output change. I am providing multiple different getter functions for states 
- * and events. How you determine the output is entirely up to you.
- * 
  ******************************************************************************/
 
 #include "Switch.h"
@@ -37,65 +22,29 @@
 #define OUTPUT_A_BIT    0
 #define OUTPUT_B_BIT    1
 
-// ***** Function Prototypes ***************************************************
+// ***** Global Variables ******************************************************
+
+
+// ***** Static Function Prototypes ********************************************
 
 static void DecodeOutput(Switch *self, SwitchState newState);
 
-// ***** Global Variables ******************************************************
+// *****************************************************************************
 
-/***************************************************************************//**
- * @brief Initializes a normal "SPDT" Switch object
- * 
- * @param self  pointer to the Switch that you are using
- * 
- * @param debounceMs  time to debounce the switch in milliseconds
- * 
- * @param tickMs  how often you plan to call the Switch Tick function
- * 
- * @param inputAInit  the initial state of input A (true = On)
- * 
- * @param inputBInit  the initial state of input B (true = On)
- */
-void Switch_Init(Switch *self, uint16_t debounceMs, uint16_t tickMs, bool inputAInit, bool inputBInit)
+void Switch_Init(Switch *self, uint16_t debounceMs, uint16_t tickMs, bool inputAInit, bool inputBInit, bool centerOffAllowed)
 {
-    Switch_InitWithCenterOff(self, debounceMs, tickMs, inputAInit, inputBInit, false);
-}
-
-/***************************************************************************//**
- * @brief Initializes a Switch object with "center off" feature
- * 
- * @param self  pointer to the Switch that you are using
- * 
- * @param debounceMs  time to debounce the switch in milliseconds
- * 
- * @param tickMs  how often you plan to call the Switch Tick function
- * 
- * @param inputAInit  the initial state of input A (true = On)
- * 
- * @param inputBInit  the initial state of input B (true = On)
- * 
- * @param offAllowed  true = both inputs off is a valid state
- */
-void Switch_InitWithCenterOff(Switch *self, uint16_t debounceMs, uint16_t tickMs, bool inputAInit, bool inputBInit, bool offAllowed)
-{
-    if(offAllowed)
-    {
+    if(centerOffAllowed)
         self->type = SW_CENTER_OFF;
-    }
     else
-    {
         self->type = SW_NORMAL;
-    }
 
     if(tickMs != 0)
-    {
         self->debouncePeriod = debounceMs / tickMs;
-    }
 
     // Check for invalid initial state
     SwitchState initState = (SwitchState)((inputAInit << OUTPUT_A_BIT) | (inputBInit << OUTPUT_B_BIT));
 
-    if(initState == SW_OUTPUT_OFF && offAllowed == false)
+    if(initState == SW_OUTPUT_OFF && centerOffAllowed == false)
     {
         initState = SW_OUTPUT_INVALID;
     }
@@ -103,15 +52,18 @@ void Switch_InitWithCenterOff(Switch *self, uint16_t debounceMs, uint16_t tickMs
     self->debounceState = SW_READY;
 }
 
-/***************************************************************************//**
- * @brief Update the Switch with its current status
- * 
- * @param self  pointer to the Switch that you are using
- * 
- * @param inputAIsOn  true if input A is on
- * 
- * @param inputBIsOn  true if input B is on
- */
+// *****************************************************************************
+
+void Switch_SetCenterOff(Switch *self, bool centerOffAllowed)
+{
+    if(centerOffAllowed)
+        self->type = SW_CENTER_OFF;
+    else
+        self->type = SW_NORMAL;
+}
+
+// *****************************************************************************
+
 void Switch_Tick(Switch *self, bool inputAIsOn, bool inputBIsOn)
 {
     SwitchState currentState = (SwitchState)((inputAIsOn << OUTPUT_A_BIT) | (inputBIsOn << OUTPUT_B_BIT));
@@ -151,15 +103,10 @@ void Switch_Tick(Switch *self, bool inputAIsOn, bool inputBIsOn)
                 self->outputState = SW_OUTPUT_INVALID;
             break;
     }
-    
 }
-/***************************************************************************//**
- * @brief Check if there was an output B on event
- * 
- * @param self  pointer to the Switch that you are using
- * 
- * @return true true if output A changed from off to on
- */
+
+// *****************************************************************************
+
 bool Switch_GetOutputAOnEvent(Switch *self)
 {
     if(self->flags.outputAOnEvent)
@@ -168,23 +115,15 @@ bool Switch_GetOutputAOnEvent(Switch *self)
         return false;
 }
 
-/***************************************************************************//**
- * @brief Clear the output A on event flag
- * 
- * @param self  pointer to the Switch that you are using
- */
+// *****************************************************************************
+
 void Switch_ClearOutputAOnEventFlag(Switch *self)
 {
     self->flags.outputAOnEvent = 0;
 }
 
-/***************************************************************************//**
- * @brief Check if there was an output B on event
- * 
- * @param self  pointer to the Switch that you are using
- * 
- * @return true if output B changed from off to on
- */
+// *****************************************************************************
+
 bool Switch_GetOutputBOnEvent(Switch *self)
 {
     if(self->flags.outputBOnEvent)
@@ -193,23 +132,15 @@ bool Switch_GetOutputBOnEvent(Switch *self)
         return false;
 }
 
-/***************************************************************************//**
- * @brief Clear the output B on event flag
- * 
- * @param self  pointer to the Switch that you are using
- */
+// *****************************************************************************
+
 void Switch_ClearOutputBOnEventFlag(Switch *self)
 {
     self->flags.outputBOnEvent = 0;
 }
 
-/***************************************************************************//**
- * @brief Check if there was an output off event
- * 
- * @param self  pointer to the Switch that you are using
- * 
- * @return true if there was an output off event
- */
+// *****************************************************************************
+
 bool Switch_GetOffEvent(Switch *self)
 {
     if(self->flags.outputOffEvent)
@@ -218,23 +149,15 @@ bool Switch_GetOffEvent(Switch *self)
         return false;
 }
 
-/***************************************************************************//**
- * @brief Clear the output off event flag
- * 
- * @param self  pointer to the Switch that you are using
- */
+// *****************************************************************************
+
 void Switch_ClearOffEventFlag(Switch *self)
 {
     self->flags.outputOffEvent = 0;
 }
 
-/***************************************************************************//**
- * @brief Check if there was an output change event
- * 
- * @param self  pointer to the Switch that you are using
- * 
- * @return true if there was an output change event
- */
+// *****************************************************************************
+
 bool Switch_OutputChangeEvent(Switch *self)
 {
     if(self->flags.switchChangedEvent)
@@ -243,23 +166,15 @@ bool Switch_OutputChangeEvent(Switch *self)
         return false;
 }
 
-/***************************************************************************//**
- * @brief Clear the output change event flag
- * 
- * @param self  pointer to the Switch that you are using
- */
+// *****************************************************************************
+
 void Switch_ClearOutputChangeEventFlag(Switch *self)
 {
     self->flags.switchChangedEvent = 0;
 }
 
-/***************************************************************************//**
- * @brief Check if the switch is currently off
- * 
- * @param self  pointer to the Switch that you are using
- * 
- * @return true if switch is off and off is allowed as a valid state
- */
+// *****************************************************************************
+
 bool Switch_IsOff(Switch *self)
 {
     if(self->type == SW_CENTER_OFF && self->outputState == SW_OUTPUT_OFF)
@@ -268,13 +183,8 @@ bool Switch_IsOff(Switch *self)
         return false;
 }
 
-/***************************************************************************//**
- * @brief Get the current state of output A
- * 
- * @param self  pointer to the Switch that you are using
- * 
- * @return true if output A is currently on
- */
+// *****************************************************************************
+
 bool Switch_OutputA(Switch *self)
 {
     if(self->outputState == SW_OUTPUT_A)
@@ -283,13 +193,8 @@ bool Switch_OutputA(Switch *self)
         return false;
 }
 
-/***************************************************************************//**
- * @brief Get the current state of output B
- * 
- * @param self  pointer to the Switch that you are using
- * 
- * @return true if output B is currently on
- */
+// *****************************************************************************
+
 bool Switch_OutputB(Switch *self)
 {
     if(self->outputState == SW_OUTPUT_B)
@@ -298,13 +203,8 @@ bool Switch_OutputB(Switch *self)
         return false;
 }
 
-/***************************************************************************//**
- * @brief Get the current output of the switch as an enum
- * 
- * @param self  pointer to the Switch that you are using
- * 
- * @return SwitchState  an enum that holds the switch state
- */
+// *****************************************************************************
+
 SwitchState Switch_GetState(Switch *self)
 {
     return(self->outputState);
@@ -347,7 +247,7 @@ static void DecodeOutput(Switch *self, SwitchState newState)
     }
 
     // If the output has become zero
-    if(stateChange != 0 && newState == 0)
+    if(stateChange != 0 && newState == SW_OUTPUT_OFF)
     {
         if(self->type == SW_CENTER_OFF)
         {
--- Switch/Switch.h
+++ Switch/Switch.h
@@ -9,9 +9,29 @@
  * 
  * @details
  *      A library that handles basic features of a "dual throw" switch. There
- * are two inputs and one output. The output A and B cannot be both true. 
- * Optional "center off" that allows output to be off. The default is to not 
- * allow the output to be off.
+ * are two inputs and one output. The output can either be A or B, but not
+ * both at the same time. There is an optional "center off" feature that allows 
+ * output to be off. The default is no center-off.
+ * 
+ *      To create a Switch, you need a Switch object, the debounce time in 
+ * milliseconds and the update rate in milliseconds. (This is how often you plan 
+ * to periodically call the tick function.) If you are using hardware to 
+ * debounce your switch inputs, you can set the debounce time to zero. You can 
+ * also set the tick rate to zero, but you still must call the Tick function,
+ * or else the output will never change. (Honestly though, if you are already
+ * debouncing your inputs, why are you using this library at all? That's pretty
+ * much the only feature here except maybe the center lockout.)
+ * 
+ *      Be sure to give the initialization function the initial value or else
+ * you will get a false event on startup. Also, if you would like to use this 
+ * for a single pole switch, simply set one of the inputs to the tick
+ * function to always be false.
+ * 
+ *      There are flags for different events such as "A on", "B on", as well as 
+ * any output change. I am providing multiple different getter functions for 
+ * states and events. How you determine the output is entirely up to you. The
+ * events flags are not cleared automatically. There may be times when you 
+ * don't want to immediately clear a flag.
  * 
  ******************************************************************************/
 
@@ -68,36 +88,149 @@ typedef struct SwitchTag
     } flags;
 } Switch;
 
-// ***** Function Prototypes ***************************************************
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+// ***** Function Prototypes *************************************************//
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
 
-void Switch_Init(Switch *self, uint16_t debounceMs, uint16_t tickMs, bool inputAInit, bool inputBInit);
+/***************************************************************************//**
+ * @brief Initializes a "SPDT" Switch object
+ * 
+ * @param self  pointer to the Switch that you are using
+ * @param debounceMs  time to debounce the switch in milliseconds
+ * @param tickMs  how often you plan to call the Switch Tick function
+ * @param inputAInit  the initial state of input A (true = On)
+ * @param inputBInit  the initial state of input B (true = On)
+ * @param centerOffAllowed  true = allow the off state to be valid
+ */
+void Switch_Init(Switch *self, uint16_t debounceMs, uint16_t tickMs, bool inputAInit, bool inputBInit, bool centerOffAllowed);
 
-void Switch_InitWithCenterOff(Switch *self, uint16_t debounceMs, uint16_t tickMs, bool inputAInit, bool inputBInit, bool offAllowed);
+/***************************************************************************//**
+ * @brief Enable or disable the "center-off" feature
+ * 
+ * This will allow you change the type of switch after initializing it, if you
+ * want to do that.
+ * 
+ * @param self  pointer to the Switch that you are using
+ * 
+ * @param centerOffAllowed  true = allow the off state to be valid
+ */
+void Switch_SetCenterOff(Switch *self, bool centerOffAllowed);
 
+/***************************************************************************//**
+ * @brief Update the Switch with its current status
+ * 
+ * @param self  pointer to the Switch that you are using
+ * 
+ * @param inputAIsOn  true if input A is on
+ * 
+ * @param inputBIsOn  true if input B is on
+ */
 void Switch_Tick(Switch *self, bool inputAIsOn, bool inputBIsOn);
 
+/***************************************************************************//**
+ * @brief Check if there was an output B on event
+ * 
+ * @param self  pointer to the Switch that you are using
+ * 
+ * @return true true if output A changed from off to on
+ */
 bool Switch_GetOutputAOnEvent(Switch *self);
 
+/***************************************************************************//**
+ * @brief Clear the output A on event flag
+ * 
+ * @param self  pointer to the Switch that you are using
+ */
 void Switch_ClearOutputAOnEventFlag(Switch *self);
 
+/***************************************************************************//**
+ * @brief Check if there was an output B on event
+ * 
+ * @param self  pointer to the Switch that you are using
+ * 
+ * @return true if output B changed from off to on
+ */
 bool Switch_GetOutputBOnEvent(Switch *self);
 
+/***************************************************************************//**
+ * @brief Clear the output B on event flag
+ * 
+ * @param self  pointer to the Switch that you are using
+ */
 void Switch_ClearOutputBOnEventFlag(Switch *self);
 
+/***************************************************************************//**
+ * @brief Check if there was an output off event
+ * 
+ * @param self  pointer to the Switch that you are using
+ * 
+ * @return true if there was an output off event
+ */
 bool Switch_GetOffEvent(Switch *self);
 
+/***************************************************************************//**
+ * @brief Clear the output off event flag
+ * 
+ * @param self  pointer to the Switch that you are using
+ */
 void Switch_ClearOffEventFlag(Switch *self);
 
+/***************************************************************************//**
+ * @brief Check if there was an output change event
+ * 
+ * @param self  pointer to the Switch that you are using
+ * 
+ * @return true if there was an output change event
+ */
 bool Switch_OutputChangeEvent(Switch *self);
 
+/***************************************************************************//**
+ * @brief Clear the output change event flag
+ * 
+ * @param self  pointer to the Switch that you are using
+ */
 void Switch_ClearOutputChangeEventFlag(Switch *self);
 
+/***************************************************************************//**
+ * @brief Check if the switch is currently off
+ * 
+ * @param self  pointer to the Switch that you are using
+ * 
+ * @return true if switch is off and off is allowed as a valid state
+ */
 bool Switch_IsOff(Switch *self);
 
+/***************************************************************************//**
+ * @brief Get the current state of output A
+ * 
+ * @param self  pointer to the Switch that you are using
+ * 
+ * @return true if output A is currently on
+ */
 bool Switch_OutputA(Switch *self);
 
+/***************************************************************************//**
+ * @brief Get the current state of output B
+ * 
+ * @param self  pointer to the Switch that you are using
+ * 
+ * @return true if output B is currently on
+ */
 bool Switch_OutputB(Switch *self);
 
+/***************************************************************************//**
+ * @brief Get the raw value of the switch as an enum
+ * 
+ * This can return the SW_OUTPUT_OFF regardless of whether or not the
+ * center-off feature is enabled. Be careful. If you want to be safer, use the
+ * OutputA, OutputB, and IsOff functions instead.
+ * 
+ * @param self  pointer to the Switch that you are using
+ * 
+ * @return SwitchState  an enum that holds the switch state
+ */
 SwitchState Switch_GetState(Switch *self);
 
-#endif	/* TRIGGER_H */
+#endif /* TRIGGER_H */
