314b5f0f2b5afbf758760073825637b7059d0251
HQ\mspinks <mspinksosu@gmail.com>
2023-01-03 12:14:52 -0600

    Beginning to add types and different formats that will be needed

--- Emulated EEPROM/IEmuEE.c
+++ Emulated EEPROM/IEmuEE.c
@@ -16,27 +16,60 @@
 
 // ***** Defines ***************************************************************
 
-#define EMUEE_PAGE_STATUS_MASK      0xFF00
-#define EMUEE_PAGE_FORMAT_MASK      0x00FF
-#define EMUEE_ENTRY_STATUS_MASK     0xC000
+#define EMUEE_PAGE_STATUS_UNKOWN    0
+#define EMUEE_PAGE_STATUS_ERASED    0xFFFF
+#define EMUEE_PAGE_STATUS_VALID     0xAAAA
+#define EMUEE_PAGE_STATUS_RECEIVE   0xCCCC
 
+#define EMUEE_ENTRY_STATUS_MASK     0xC000
 
 // ***** Global Variables ******************************************************
 
-enum EmuPageStatus
+/* The first eight bytes of the flash page will contain a header which has 
+information about the page status and its format. */
+enum
 {
-    EMUEE_PAGE_STATUS_UNKOWN = 0,
-    // TODO
+    EMUEE_PAGE_STATUS_BYTE_LOW = 0,
+    EMUEE_PAGE_STATUS_BYTE_HIGH,
+    EMUEE_PAGE_FORMAT_BYTE,
+    EMUEE_PAGE_PAD_0,
+    EMUEE_PAGE_SIZE_IN_BYTES_LOW,
+    EMUEE_PAGE_SIZE_IN_BYTES_HIGH,
+    EMUEE_PAGE_WORD_SIZE_BYTE,
+    EMUEE_PAGE_PAD_1,
 };
 
-enum EmuEntryStatus
+/* The first two bytes of an entry will be a header which contains its status
+and its size. Bits [15:14] of an entry will hold the status bits. Bits [13:0] 
+will be X.Y, where X is the virtual address and Y length of the entry in bytes 
+(both right justified) */
+enum
+{
+    EMUEE_ENTRY_HEADER_LOW = 0,
+    EMUEE_ENTRY_HEADER_HIGH,
+};
+
+enum EmuEntryStatus // TODO leave as enum or change to define?
 {
     EMUEE_ENTRY_STATUS_UNKOWN = 0,
-    // TODO
+    EMUEE_ENTRY_STATUS_VALID,
+    EMUEE_ENTRY_STATUS_ERASED,
 };
+
+static EmuEEFormat emueeFormat = EMUEE_FORMAT_7_7;
+static EmuEEFlashWordSize emueeFlashWordSize = EMUEE_WORD_SIZE_2_BYTES;
+static uint16_t emueePageSizeInBytes = EMUEE_PAGE_SIZE_1KB;
+static bool littleEndian = true;
+static uint32_t emueePage0Address, emueePage1Address;
+
+// TODO check virtual address against format and page size to find end of page
+
 
 // ***** Static Function Prototypes ********************************************
 
+static enum EmuEntryStatus GetEntryStatus(uint16_t header);
+static uint16_t GetEntryAddress(uint16_t header);
+static uint16_t GetEntrySize(uint16_t header);
 
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                            //
@@ -44,7 +77,63 @@ enum EmuEntryStatus
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
+EmuEEError EmuEE_Init(uint32_t page0Address, uint32_t page1Address, 
+    uint16_t pageSizeInBytes, EmuEEFlashWordSize flashWordSize, bool isLittleEndian)
+{
+    // TODO check if address is in valid range
+    if(page0Address == page1Address || page0Address == 0 || page1Address == 0
+        || pageSizeInBytes < EMUEE_WORD_SIZE_2_BYTES)
+    {
+        return EMUEE_ERROR_INVALID_ADDRESS;
+    }
 
+    emueePage0Address = page0Address;
+    emueePage1Address = page1Address;
+    emueePageSizeInBytes = pageSizeInBytes;
+    emueeFlashWordSize = flashWordSize;
+    littleEndian = isLittleEndian;
+
+    return EMUEE_ERROR_NONE;
+}
+
+EmuEEError EmuEE_GetFormat(uint32_t pageAddress, EmuEEFormat *retFormatType)
+{
+    // TODO check if address is valid
+    uint16_t pageHeader = 0;
+
+    // Read first eight bytes
+
+    // Reorder bytes into words
+
+    // Check status and check if format is valid
+}
+
+void EmuEE_Format(uint32_t page0Address, uint32_t page1Address, EmuEEFormat format)
+{
+    emueeFormat = format;
+
+    // Write the page header
+}
+
+uint16_t EmuEE_AddEntry(uint8_t *src, uint16_t size)
+{
+    // returns virtual address
+} 
+
+uint16_t EmuEE_GetEntrySizeBytes(uint16_t virtualAddress)
+{
+
+}
+
+EmuEEError EmuEE_Read(uint16_t virtualAddress, uint8_t *dst, uint16_t dstSize)
+{
+
+}
+
+EmuEEError EmuEE_Write(uint16_t virtualAddress, uint8_t *src, uint16_t srcSize)
+{
+
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                            //
@@ -53,9 +142,27 @@ enum EmuEntryStatus
 ////////////////////////////////////////////////////////////////////////////////
 
 
-
 // *****************************************************************************
 
+
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+// ***** Local Functions *****************************************************//
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+static enum EmuEntryStatus GetEntryStatus(uint16_t header)
+{
+    return ((header & EMUEE_ENTRY_STATUS_MASK) >> 14);
+}
+
+static bool IsEntryFormatValid(uint16_t header)
+{
+    /* Check the entry format. All entries use 14 bits. Format_13_1 is not a 
+    valid format because a flash word is going to be at least two bytes long
+    possibly more. */
+
+}
 
 /*
  End of File
--- Emulated EEPROM/IEmuEE.h
+++ Emulated EEPROM/IEmuEE.h
@@ -20,6 +20,9 @@
 
 // ***** Defines ***************************************************************
 
+#define EMUEE_PAGE_SIZE_1KB     0x400
+#define EMUEE_PAGE_SIZE_2KB     0x800
+#define EMUEE_PAGE_SIZE_4KB     0x1000
 
 // ***** Global Variables ******************************************************
 
@@ -29,21 +32,27 @@ typedef enum EmuEEFormatTag
     X = num of entries (2^X)
     Y = max size of each entry (2^Y bytes)
     Total is 14 bits. Bits [15:14] are reserved 
-    Your flash may not support the desired number of entries.
-    Choose accordingly.*/
-    EMUEE_FRMT_12_2 = 0xC2,
-    EMUEE_FRMT_11_3 = 0xB3,
-    EMUEE_FRMT_10_4 = 0xA4,
-    EMUEE_FRMT_9_5 = 0x95,
-    EMUEE_FRMT_8_6 = 0x86,
-    EMUEE_FRMT_7_7 = 0x77, // default
-    EMUEE_FRMT_6_8 = 0x68,
-    EMUEE_FRMT_5_9 = 0x59,
-    EMUEE_FRMT_4_10 = 0x4A,
-    EMUEE_FRMT_3_11 = 0x3B,
-    EMUEE_FRMT_2_12 = 0x2C,
-    EMUEE_FRMT_1_13 = 0x1D,
+    Your flash page may not be able to hold the desired number of entries
+    depending on its size. Choose accordingly.*/
+    EMUEE_FORMAT_12_2 = 0xC2,
+    EMUEE_FORMAT_11_3 = 0xB3,
+    EMUEE_FORMAT_10_4 = 0xA4,
+    EMUEE_FORMAT_9_5 = 0x95,
+    EMUEE_FORMAT_8_6 = 0x86,
+    EMUEE_FORMAT_7_7 = 0x77, // default
+    EMUEE_FORMAT_6_8 = 0x68,
+    EMUEE_FORMAT_5_9 = 0x59,
+    EMUEE_FORMAT_4_10 = 0x4A,
+    EMUEE_FORMAT_3_11 = 0x3B,
+    EMUEE_FORMAT_2_12 = 0x2C,
+    EMUEE_FORMAT_1_13 = 0x1D,
 } EmuEEFormat;
+
+typedef enum EmuEEFlashWordSizeTag
+{
+    EMUEE_WORD_SIZE_2_BYTES = 2, // default
+    EMUEE_WORD_SIZE_4_BYTES = 4,
+} EmuEEFlashWordSize;
 
 typedef enum EmuEEErrorTag
 {
@@ -65,11 +74,12 @@ typedef enum EmuEEErrorTag
 //                                                                            //
 ////////////////////////////////////////////////////////////////////////////////
 
-EmuEEError EmuEE_Init(uint32_t page0Address, uint32_t page1Address);
+EmuEEError EmuEE_Init(uint32_t page0Address, uint32_t page1Address, 
+    uint16_t pageSizeInBytes, EmuEEFlashWordSize flashWordSize, bool isLittleEndian);
 
 EmuEEError EmuEE_GetFormat(uint32_t pageAddress, EmuEEFormat *retFormatType);
 
-void EmuEE_Format(uint32_t page0Address, uint32_t page1Address, EmuEEFormat);
+void EmuEE_Format(uint32_t page0Address, uint32_t page1Address, EmuEEFormat format);
 
 uint16_t EmuEE_AddEntry(uint8_t *src, uint16_t size); // returns virtual address
 
